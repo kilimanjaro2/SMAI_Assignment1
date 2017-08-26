91c7334a901ac35365c66696a14ebdf0037a490f
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2017-08-26 14:18:58 +0530

    added q3.py starter code

---
+++ q3.py
@@ -0,0 +1,13 @@
+#kilimanjaro2 submitting q3
+#This is Anfield. YNWA
+#201564134
+import numpy as np
+import scipy as sc
+import math
+import sys
+import csv
+
+input = np.genfromtxt(argv[1], delimiter = ',')
+
+print (input.shape)
+#op : 11251 , 10
