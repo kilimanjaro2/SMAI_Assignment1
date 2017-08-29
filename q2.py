b1834c5cf02cd8e16b456a49c3588321c175c5fb
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2017-08-29 19:50:37 +0530

    Final q2.py

--- q2.py
+++ q2.py
@@ -11,48 +11,81 @@ arg_check = 0
 if(sys.argv[0]=="time"):
     arg_check += 1
 train_name = str(sys.argv[1+arg_check])
-#test_name = str(sys.argv[2+arg_check])
+test_name = str(sys.argv[2+arg_check])
 
 unproc_input = np.genfromtxt(train_name, delimiter = ',')
 train_x_max = unproc_input.shape[0]
 size_x_max = train_x_max
 
+test_data = np.genfromtxt(test_name, delimiter = ',')
+size_x_max = test_data.shape[0]
 train_x_max *= 4
 train_x_max /= 5
 
 find_median = 0
 median_cnt = 0
 
-learning_rate = 0.3
 
 train_flag = unproc_input[:train_x_max,[10]]
-test_flag = unproc_input[train_x_max:,[10]]
 train_pruned = unproc_input[:train_x_max,1:10]
-test_pruned = unproc_input[train_x_max:,1:10]
+test_pruned = test_data[:,1:10]
 
-
-for i in xrange(train_x_max):
-    if not(math.isnan(train_pruned[i][5])):
-        find_median += train_pruned[i][5]
-        median_cnt += 1
-
-median_fin = find_median / median_cnt
-
-for i in xrange(train_x_max):
-    if (math.isnan(train_pruned[i][5])):
-        train_pruned[i][5] = median_fin
-
-for i in xrange(train_x_max,size_x_max):
-    if (math.isnan(test_pruned[i-train_x_max][5])):
-        test_pruned[i-train_x_max][5] = median_fin
 
 weight = np.zeros(9)
-batch_size = 10
+batch_size = 100
 margin = 10
+nyet = 0.05
 
-for i in xrange(train_x_max):
+for j in xrange(batch_size):
+    for i in xrange(train_x_max):
+        #print test_pruned[i].shape
+        #print i
+        tot = np.dot(train_pruned[i],weight.T)
+        denom = np.dot(train_pruned[i],train_pruned[i].T)
+        mult = (nyet * (margin - tot) / denom)
+        if tot <= margin and train_flag[i] == 4 :
+            weight += (mult * train_pruned[i])
+        elif tot >= margin and train_flag[i] == 2:
+            weight += (mult * train_pruned[i])
+
+
+for i in xrange(size_x_max):
     tot = np.dot(test_pruned[i],weight.T)
-    if tot >= 0 and train_flag == 2 :
+    if(tot >= margin):# and test_flag[i] == 1):
+        print (4)
+    elif(tot < margin):# and test_flag[i] == 0):
+        print (2)
 
-    elif tot < 0 and train_flag == 4:
-        #do something else
+
+weight = np.zeros(9)
+batch_size = 100
+margin = 10
+nyet = 0.05
+minima = 999999999
+
+for j in xrange(batch_size):
+    wrong=0
+    for i in xrange(train_x_max):
+        #print test_pruned[i].shape
+        #print i
+        tot = np.dot(train_pruned[i],weight.T)
+        denom = np.dot(train_pruned[i],train_pruned[i].T)
+        mult = (nyet * (margin - tot) / denom)
+        if tot <= margin and train_flag[i] == 4 :
+            weight += (mult * train_pruned[i])
+            wrong +=1
+        elif tot >= margin and train_flag[i] == 2:
+            weight += (mult * train_pruned[i])
+            wrong +=1
+    if(wrong < minima):
+        minima = wrong
+        final = weight
+
+weight = final
+
+for i in xrange(size_x_max):
+    tot = np.dot(test_pruned[i],weight.T)
+    if(tot >= margin):# and test_flag[i] == 1):
+        print (4)
+    elif(tot < margin):# and test_flag[i] == 0):
+        print (2)
