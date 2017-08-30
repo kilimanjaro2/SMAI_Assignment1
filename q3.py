abdd21030a7cdc21d272a24dbc183c00953fecfc
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2017-08-30 10:20:59 +0530

    final q3.py

--- q3.py
+++ q3.py
@@ -1,181 +1,107 @@
-import csv
-import collections
+import math
+import pandas as pd
 import sys
+from pandas import DataFrame
+from collections import Counter
 import numpy as np
-import re
 
-class DecisionTree:
-	"""Binary tree implementation with true and false branch. """
-	def __init__(self, col=-1, value=None, trueBranch=None, falseBranch=None, results=None):
-		self.col = col
-		self.value = value
-		self.trueBranch = trueBranch
-		self.falseBranch = falseBranch
-		self.results = results # None for nodes, not None for leaves
 
-def divideSet(rows, column, value):
-	splittingFunction = None
-	if isinstance(value, int) or isinstance(value, float): # for int and float values
-		splittingFunction = lambda row : row[column] >= value
-	else: # for strings
-		splittingFunction = lambda row : row[column] == value
-	list1 = [row for row in rows if splittingFunction(row)]
-	list2 = [row for row in rows if not splittingFunction(row)]
-	return (list1, list2)
+def generate_input(df):
+    manipulate(df)
+    typecast(df)
 
-def uniqueCounts(rows):
-	results = {}
-	for row in rows:
-		r = row[-1]
-		if r not in results: results[r] = 0
-		results[r] += 1
-	return results
+def manipulate(df):
+    df.loc[:,'satisfaction_level'] *= 100
+    df.loc[:,'satisfaction_level'] /= 10
+    df.loc[:,'last_evaluation'] *= 100
+    df.loc[:,'last_evaluation'] /= 10
+    df.loc[:,'average_montly_hours'] /= 25
 
-def entropy(rows):
-	from math import log
-	log2 = lambda x: log(x)/log(2)
-	results = uniqueCounts(rows)
-	entr = 0.0
-	for r in results:
-		p = float(results[r])/len(rows)
-		entr -= p*log2(p)
-	return entr
+def typecast(df):
+    df.loc[:,'satisfaction_level'] = df.loc[:,'satisfaction_level'].astype(int)
+    df.loc[:,'last_evaluation'] = df.loc[:,'last_evaluation'].astype(int)
+    df.loc[:,'average_montly_hours'] = df.loc[:,'average_montly_hours'].astype(int)
 
-def variance(rows):
-	if len(rows) == 0: return 0
-	data = [float(row[len(row) - 1]) for row in rows]
-	mean = sum(data) / len(data)
-	variance = sum([(d-mean)**2 for d in data]) / len(data)
-	return variance
 
-def growDecisionTreeFrom(rows, evaluationFunction=entropy):
-	"""Grows and then returns a binary decision tree.
-	evaluationFunction: entropy or gini"""
-	if len(rows) == 0: return DecisionTree()
-	currentScore = evaluationFunction(rows)
-	bestGain = 0.0
-	bestAttribute = None
-	bestSets = None
-	columnCount = len(rows[0]) - 1  # last column is the result/target column
-	for col in range(0, columnCount):
-		columnValues = [row[col] for row in rows]
+def net_entropy(a_list):
+    cnt = Counter(x for x in a_list)
+    num_instances = len(a_list)
+    probs = [x /(num_instances * 1.0) for x in cnt.values()]
+    total = 0.0
+    for prob in probs:
+        if prob != 0:
+            total += -prob*math.log(prob, 2)
+    return total
 
-		for value in columnValues:
-			if value != 'nan':
-				(set1, set2) = divideSet(rows, col, value)
-				p = float(len(set1)) / len(rows)
-				gain = currentScore - p*evaluationFunction(set1) - (1-p)*evaluationFunction(set2)
-				#print gain
-				#print bestGain
-				if gain>bestGain and len(set1)>0 and len(set2)>0:
-					bestGain = gain
-					bestAttribute = (col, value)
-					bestSets = (set1, set2)
+def information_gain(df, split_attribute_name, target_attribute_name, trace=0):
+    df_split = df.groupby(split_attribute_name)
 
-	if bestGain > 0:
-		trueBranch = growDecisionTreeFrom(bestSets[0])
-		falseBranch = growDecisionTreeFrom(bestSets[1])
-		return DecisionTree(col=bestAttribute[0], value=bestAttribute[1], trueBranch=trueBranch, falseBranch=falseBranch)
-	else:
-		return DecisionTree(results=uniqueCounts(rows))
+    # Calculate Entropy for Target Attribute, as well as Proportion of Obs in Each Data-Split
+    #nobs = len(df.index) * 1.0
+    df_agg_ent = df_split.agg({target_attribute_name : [net_entropy, lambda x: len(x)/float(len(df.index) * 1.0)] })[target_attribute_name]
+    df_agg_ent.columns = ['Entropy', 'PropObservations']
+    # Calculate Information Gain:
+    return net_entropy(df[target_attribute_name]) - sum( df_agg_ent['Entropy'] * df_agg_ent['PropObservations'] )
 
-def prune(tree, minGain, evaluationFunction=entropy, notify=False):
-	"""Prunes the obtained tree according to the minimal gain (entropy or Gini). """
-	# recursive call for each branch
-	if tree.trueBranch.results == None: prune(tree.trueBranch, minGain, evaluationFunction, notify)
-	if tree.falseBranch.results == None: prune(tree.falseBranch, minGain, evaluationFunction, notify)
+def build_tree(df, target_attribute_name, attribute_names, default_class=None):
+    cnt = Counter(x for x in df[target_attribute_name])
+    if len(cnt) == 1:
+        return cnt.keys()[0]
 
-	# merge leaves (potentionally)
-	if tree.trueBranch.results != None and tree.falseBranch.results != None:
-		tb, fb = [], []
+    if df.empty or (not attribute_names):
+        return default_class
 
-		for v, c in tree.trueBranch.results.items(): tb += [[v]] * c
-		for v, c in tree.falseBranch.results.items(): fb += [[v]] * c
+    index_of_max = cnt.values().index(max(cnt.values()))
+    default_class = cnt.keys()[index_of_max]
+    gainz = [information_gain(df, attr, target_attribute_name) for attr in attribute_names]
+    #index_of_max = gainz.index(max(gainz))
+    best_attr = attribute_names[gainz.index(max(gainz))]
+    tree = {best_attr:{}}
+    remaining_attribute_names = [i for i in attribute_names if i != best_attr]
 
-		p = float(len(tb)) / len(tb + fb)
-		delta = evaluationFunction(tb+fb) - p*evaluationFunction(tb) - (1-p)*evaluationFunction(fb)
-		if delta < minGain:
-			if notify: print('A branch was pruned: gain = %f' % delta)
-			tree.trueBranch, tree.falseBranch = None, None
-			tree.results = uniqueCounts(tb + fb)
+    for attr_val, data_subset in df.groupby(best_attr):
+        subtree = build_tree(data_subset,
+                    target_attribute_name,
+                    remaining_attribute_names,
+                    default_class)
+        tree[best_attr][attr_val] = subtree
+    return tree
 
-def classify(observations, tree, dataMissing=False):
-	"""Classifies the observationss according to the tree.
-	dataMissing: true or false if data are missing or not. """
-
-	def classifyWithoutMissingData(observations, tree):
-		if tree.results != None:  # leaf
-			return tree.results
-		else:
-			v = observations[tree.col]
-			branch = None
-			if isinstance(v, int) or isinstance(v, float):
-				if v >= tree.value: branch = tree.trueBranch
-				else: branch = tree.falseBranch
-			else:
-				if v == tree.value: branch = tree.trueBranch
-				else: branch = tree.falseBranch
-		return classifyWithoutMissingData(observations, branch)
-
-	return classifyWithoutMissingData(observations, tree)
-
-def plot(decisionTree):
-	"""Plots the obtained decision tree. """
-	def toString(decisionTree, indent=''):
-		if decisionTree.results != None:  # leaf node
-			return str(decisionTree.results)
-		else:
-			if isinstance(decisionTree.value, int) or isinstance(decisionTree.value, float):
-				decision = 'Column %s: x >= %s?' % (decisionTree.col, decisionTree.value)
-			else:
-				decision = 'Column %s: x == %s?' % (decisionTree.col, decisionTree.value)
-			trueBranch = indent + 'yes -> ' + toString(decisionTree.trueBranch, indent + '\t')
-			falseBranch = indent + 'no  -> ' + toString(decisionTree.falseBranch, indent + '\t')
-			return (decision + '\n' + trueBranch + '\n' + falseBranch)
-
-	print(toString(decisionTree))
+def classify(instance, tree, default=None):
+    attribute = tree.keys()[0]
+    if instance[attribute] in tree[attribute].keys():
+        if isinstance(tree[attribute][instance[attribute]], dict):
+            x = classify(instance, tree[attribute][instance[attribute]])
+            return x
+        else:
+            return tree[attribute][instance[attribute]]
+    else:
+        return default
 
 if __name__ == '__main__':
 
-	input_name = str(sys.argv[1])
-	unproc_data = np.genfromtxt(input_name,delimiter=",",dtype=None)
-	dump = np.copy(unproc_data[:,6])
-	unproc_data[:, 6] = unproc_data[:, 9]
-	unproc_data[:, 9] = dump
-	training_size = unproc_data.shape[0]
-	i = 0
-	while i < training_size:
-		if unproc_data[i][6] == "low" :
-			unproc_data[i][6] = float(0.0)
-		elif unproc_data[i][6] == "medium" :
-			unproc_data[i][6] = float(1.0)
-		elif unproc_data[i][6] == "high" :
-			unproc_data[i][6] = float(2.0)
-		i += 1
-	total_size = training_size
-	training_size *= 3
-	training_size /= 5
-	training_size = 2000 # ***************************************** #
-	#total_size = 30
-	trainingData = unproc_data[:training_size,:]
-	testData = unproc_data[training_size:, :9]
-	testFlag = unproc_data[training_size:, 9]
-	decisionTree = growDecisionTreeFrom(trainingData)
-	cnt = 0
-	prune(decisionTree, 0.5, notify=False)
-	#plot(decisionTree)
-	i = training_size
-	while i < total_size:
-		dump_str = str(classify(testData[i - training_size], decisionTree))
-		#print dump_str #.split()
-		temp_str = re.sub('[^0-9a-zA-Z]+', ' ', dump_str)
-		dump_str = temp_str.split()
-		dump_length = len(dump_str)
-		if dump_length == 4:
-			if(dump_str[1] < dump_str[3]):
-				dump_str[0] = dump_str[2]
-		if(int(testFlag[i-training_size]) == int(dump_str[0])):
-			cnt += 1
-		i += 1
-	print cnt
-	print 100 * float(cnt)/float(total_size-training_size)
+    #Loading data from data_frame
+    train_data = DataFrame.from_csv(sys.argv[1], index_col=None)
+    test = DataFrame.from_csv(sys.argv[2], index_col=None)
+
+    attribute_names = list(train_data.columns)
+    attribute_names.remove('left')
+
+    training_data = train_data
+    test_data  = test
+
+    generate_input(training_data)
+    generate_input(test_data)
+
+    #Create Training Tree
+    train_tree = build_tree(training_data, 'left', attribute_names)
+    #Creating test_data
+    test_data['prediction'] = test_data.apply(classify, axis=1, args=(train_tree,0) )
+    #Final Test_data
+    test_data.fillna(0, inplace=True)
+
+    for i in test_data['prediction']:
+        op = int(i)
+        #print op
+
+    print 'Accuracy is ' + str( sum(test_data['left']==test_data['prediction'] ) / (1.0*len(test_data.index)) )
